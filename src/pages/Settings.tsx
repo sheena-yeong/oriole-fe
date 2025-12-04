3bb7744b17bc44d8b1f761787da886af36b9f243
sheena <syeongsn@gmail.com>
2025-12-04 20:36:43 +0800

    chore: added dropdownmenu package and insert placeholders

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,31 +1,73 @@
-import { useState } from 'react';
-import Dialog from '../components/User/SignUpDialog';
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import { IoMdSettings } from "react-icons/io";
 
-
-function Settings() {
-  const [openDialog, setOpenDialog] = useState<boolean>(false);
-
+export default function Settings() {
   return (
-    <>
-      <button
-        className="
-        px-4 py-2 
-        border border-yellow-500/40 
-        rounded-sm 
-        text-sm
-        hover:bg-yellow-500/10
-        transition
-        bg-[#5c5c5c]
-        font-semibold
-        text-white
-      "
-        onClick={() => setOpenDialog(true)}
-      >
-        Sign Up
-      </button>
-      <Dialog openDialog={openDialog} setOpenDialog={setOpenDialog} />
-    </>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <button className="p-2 transition text-neutral-300 hover:text-[#fe5914]">
+          <IoMdSettings size={26} />
+        </button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          side="bottom"
+          align="end"
+          className="
+            z-99
+            min-w-[180px]
+            rounded-xl
+            bg-neutral-900/90
+            backdrop-blur-xl
+            border border-neutral-800
+            shadow-[0_8px_20px_rgba(0,0,0,0.35)]
+            p-2
+            text-neutral-200
+          "
+        >
+          <DropdownMenu.Item
+            className="
+              px-3 py-2 
+              rounded-lg 
+              hover:bg-neutral-800/60 
+              transition 
+              cursor-pointer
+            "
+          >
+            Profile
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Item
+            className="
+              px-3 py-2 
+              rounded-lg 
+              hover:bg-neutral-800/60 
+              transition 
+              cursor-pointer
+            "
+          >
+            Settings
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Separator className="my-2 h-px bg-neutral-700" />
+
+          <DropdownMenu.Item
+            className="
+              px-3 py-2 
+              rounded-lg 
+              hover:bg-neutral-800/60 
+              text-[#fe5914]
+              font-medium 
+              transition 
+              cursor-pointer
+            "
+          >
+            Logout
+          </DropdownMenu.Item>
+
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
   );
 }
-
-export default Settings;
