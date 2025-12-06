2cc289c13c82e15ecfb72beaa00ccd6e0e093e14
sheena <syeongsn@gmail.com>
2025-12-07 00:14:14 +0800

    feat: added post and get watchlist coins function and changed return format for fetching coins from coingecko to include success flag for better error handling

--- src/services/crypto.ts
+++ src/services/crypto.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import type { watchListCoin } from '../../types/coins';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
@@ -9,8 +10,49 @@ export const getCoins = async (token: string | null) => {
         Authorization: `Bearer ${token}`,
       },
     });
-    return res.data;
+    return { success: true, data: res.data };
   } catch (err) {
-    console.error(err);
+    console.log('Failed to fetch coins', err);
+    return {
+      success: false,
+      error: err instanceof Error ? err.message : 'Unable to fetch coins.',
+    };
+  }
+};
+
+export const addWatchListCoins = async (
+  token: string | null,
+  coins: watchListCoin[]
+) => {
+  try {
+    const res = await axios.post(`${BASE_URL}/coins/watchlist`, coins, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return { success: true, data: res.data };
+  } catch (err) {
+    console.error('Failed to add watchlist coins:', err);
+    return {
+      success: false,
+      error: err instanceof Error ? err.message : 'An error occurred.',
+    };
+  }
+};
+
+export const getWatchListCoins = async (token: string | null) => {
+  try {
+    const res = await axios.get(`${BASE_URL}/coins/watchlist`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return { success: true, data: res.data };
+  } catch (err) {
+    console.log('Failed to fetch coins from watchlist', err);
+    return {
+      success: false,
+      error: err instanceof Error ? err.message : 'Unable to fetch coins.',
+    };
   }
 };
